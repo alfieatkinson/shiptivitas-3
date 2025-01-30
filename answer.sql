4363931ed0cd86a779c755dd4b051398952c7156
alfieatkinson <alf.atkinson13@gmail.com>
2025-01-30 14:05:42 +0000

    Add SQL queries for daily user count before and after feature change

--- answer.sql
+++ answer.sql
@@ -2,17 +2,22 @@
 
 -- PART 1: Create a SQL query that maps out the daily average users before and after the feature change
 
+-- Before the feature change
 SELECT
     DATE(login_timestamp, 'unixepoch') AS date,
     COUNT(DISTINCT user_id) AS daily_user_count
 FROM login_history
-WHERE login_timestamp < 1527897600 -- Unix timestamp for 2018-06-02
+WHERE login_timestamp <= 1527897600 -- Unix timestamp for 2018-06-02
+GROUP BY date
+ORDER BY date;
+
+-- After the feature change
+SELECT
+    DATE(login_timestamp, 'unixepoch') AS date,
+    COUNT(DISTINCT user_id) AS daily_user_count
+FROM login_history
+WHERE login_timestamp > 1527897600 -- Unix timestamp for 2018-06-02
 GROUP BY date
 ORDER BY date;
 
 -- PART 2: Create a SQL query that indicates the number of status changes by card
-
-
-
-
-
